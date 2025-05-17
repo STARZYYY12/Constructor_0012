3e400c9679f98c499ba1c872201d0b917a0aee5a
STARZYYY12 <rizkyfaiz324@gmail.com>
2025-05-17 15:38:26 +0700

    void setId dan printAll

--- mbrStatikfunc.cpp
+++ mbrStatikfunc.cpp
@@ -19,3 +19,17 @@ class mahasiswa{
 
     
 };
+
+int mahasiswa::nim=0;
+
+    void mahasiswa::setId(){
+        id = ++nim;
+    }
+
+    void mahasiswa::printAll(){
+        cout << "ID = " << id << endl;
+        cout << "Nama = " << nama << endl;
+        cout << endl;
+    }
+
+    
