02f398103b42cca99e7e7cdbbb896fc808059ed7
STARZYYY12 <rizkyfaiz324@gmail.com>
2025-05-17 15:22:21 +0700

    implementasi

--- chaifunction.cpp
+++ chaifunction.cpp
@@ -7,3 +7,13 @@ class buku
 
 public:
     buku setJudul(string judul)
+    {
+        this->judul = judul;
+        return *this; // chain function
+    }
+    string getJudul()
+    {
+        return this->judul;
+    }
+} bukunya;
+
